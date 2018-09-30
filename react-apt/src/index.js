181089414bd31e3ca97ae47e71872be15e34ae15
Kevz <kevcramirez@gmail.com>
2018-09-30 22:14:29 +0800

    https://github.com/vaanskie/React.git

---
+++ react-apt/src/index.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const element = <h1>Hellow pakyu</h1>;
+// ReactDOM.render(element, document.getElementById("root"));
+// const square = number => number * number;
+const square = number => number * number;
+console.log(square(5));
